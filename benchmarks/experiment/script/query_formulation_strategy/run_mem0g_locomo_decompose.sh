9541be508623a86f2cbea618bd704023157b2ef3
KimmoZAG <995496585@qq.com>
2026-01-17 15:35:15 +0000

    update for all

--- benchmarks/experiment/script/query_formulation_strategy/run_mem0g_locomo_decompose.sh
+++ benchmarks/experiment/script/query_formulation_strategy/run_mem0g_locomo_decompose.sh
@@ -29,7 +29,7 @@ TASK_IDS=(
 
 # 创建日志目录结构
 DATASET="locomo"
-MEMORY_NAME="PreRetrieve_Mem0g_decompose"
+MEMORY_NAME="PreRetrieval_Mem0g_decompose"
 LOG_BASE_DIR="$PROJECT_ROOT/.sage/output/benchmarks/benchmark_memory/$DATASET/$MEMORY_NAME"
 mkdir -p "$LOG_BASE_DIR"
 
