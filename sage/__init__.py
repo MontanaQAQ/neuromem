0850d1a29acd7bb5301b0cf0d6089e29d019bf08
shuhao zhang <shuhao_zhang@hust.edu.cn>
2026-03-02 21:59:30 +0800

    fix(namespace): remove legacy root marker and enable namespace discovery

--- sage/__init__.py
+++
@@ -1,6 +0,0 @@
-"""SAGE namespace package
-
-This is the root namespace for all SAGE components.
-NeuroMem is part of the SAGE ecosystem and lives under sage.neuromem.
-"""
-__path__ = __import__("pkgutil").extend_path(__path__, __name__)
